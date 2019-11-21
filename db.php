bca658c16194f70c28e145513e73ce8dfec22f00
Drew Moffatt <49130532+drewmoffatt@users.noreply.github.com>
2019-11-20 19:16:47 -0600

    Add db.php file from upload

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+
+define("MYSQL_HOST", "147.134.0.59");
+define("MYSQL_USER", "USERNAME");
+define("MYSQL_PASSWORD", "PASSWORD");
+define("MYSQL_NAME", "999group1c");
+
+$con = mysqli_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_NAME);
+?>
